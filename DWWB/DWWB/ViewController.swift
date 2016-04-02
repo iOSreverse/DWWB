f99418de27724b9e11675f73dbd1ac43c47286f0
iOSreverse <iosdevhub@gmail.com>
2016-04-02 23:41:34 +0800

    初始化项目

---
+++ DWWB/DWWB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DWWB
+//
+//  Created by xmg on 16/4/2.
+//  Copyright © 2016年 NewDee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
