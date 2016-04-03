f08ddcc62d319ca84967f7cc6b57193b5550e07d
iOSreverse <iosdevhub@gmail.com>
2016-04-04 01:32:14 +0800

    storyboard reference

--- DWWB/DWWB/AppDelegate.swift
+++ DWWB/DWWB/AppDelegate.swift
@@ -19,12 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        设置全局颜色
         UITabBar.appearance().tintColor = UIColor.orangeColor()
 
-//        创建window
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window?.rootViewController = MainViewController()
-        window?.makeKeyAndVisible()
-
-
 
         return true
     }
--- DWWB/DWWB/Classes/Main(主要)/MainViewController.swift
+++ DWWB/DWWB/Classes/Main(主要)/MainViewController.swift
@@ -13,77 +13,29 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        1.取出json文件
-        let jsonPath = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)!
-
-
-//        2.读取json数据
-        guard let jsonData = NSData(contentsOfFile: jsonPath) else {
-            return
-        }
-//        3.将data转成字典
-        guard let dicts = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: .MutableLeaves) as! [[String : AnyObject]] else {
-            return
-        }
-
-//        4.遍历字典
-        for dict in dicts {
-//            1.取出控制器的名称
-            guard let childVcName = dict["vcName"] as? String else {
-                continue
-            }
-//            2.取出标题
-            guard let title = dict["title"] as? String else {
-                continue
-            }
-//            3.取出图片名称
-            guard let imageName = dict["imageName"] as? String else {
-                continue
-            }
-
-            addChildViewController(childVcName, title: title, imageName: imageName)
-
-        }
-
-
 
 
     }
 
-    private func addChildViewController(childVcName: String, title : String, imageName : String) {
 
-//        1.获取命名空间
-        guard let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String else{
-            print("没有获取到命名空间")
-            return
+
+
+}
+
+
+extension MainViewController {
+
+    ///调整的tabbar
+    private func adjustItems() {
+        for i in 0..<tabBar.items!.count {
+//            1.取出item
+            let item = tabBar.items![i]
+
+//            2.
+
+
+
         }
 
-//        2.创建子控制器
-        guard let AnyClass = NSClassFromString(nameSpace + "." + childVcName) else {
-            print("没有创建出对应的class")
-            return
-        }
-//        3.通过class创建对象
-        guard let ChildVcClass = AnyClass as? UIViewController.Type else {
-            print("没有转成控制器")
-            return
-        }
-
-//        4.创建子控制器对象
-        let childVc = ChildVcClass.init()
-
-//        5.设置子控制器的属性
-        childVc.title = title
-        childVc.tabBarItem.image = UIImage(named: imageName)
-        childVc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-
-//        6.包装导航栏控制器
-        let childNav = UINavigationController(rootViewController: childVc)
-
-//        7.添加控制器
-        addChildViewController(childNav)
     }
-
-
-
 }
