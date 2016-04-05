8061e77798aa54ee4ebea01136665f3dbcb36b21
iOSreverse <iosdevhub@gmail.com>
2016-04-05 22:06:29 +0800

    调整Tabbar中items

--- DWWB/DWWB/Classes/Main(主要)/MainViewController.swift
+++ DWWB/DWWB/Classes/Main(主要)/MainViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class MainViewController: UITabBarController {
 
+    // MARK: - 懒加载属性
+    private lazy var imageNames = ["tabbar_home", "tabbar_message_center", "","tabbar_discover", "tabbar_profile"]
 
+
+    // MARK: - 系统回调函数
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
-
     }
+
+
+
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        //        1.遍历所有的item
+        for i in 0..<tabBar.items!.count {
+            //            2.获取item
+            let item = tabBar.items![i]
+            print("\(item)")
+
+            //            3.如果下标是2,则该item不可以和用户交互
+            if i == 2 {
+                item.enabled = false
+                continue
+            }
+
+            //            4.设置其他item的选中时候的图片
+            item.selectedImage = UIImage(named: imageNames[i] + "_highlighted")
+            
+        }
+        
+        
+}
 
 }
