9908dbafcc9e8faf48dd2e5fdc5177f1d356b20e
iOSreverse <iosdevhub@gmail.com>
2016-04-05 19:44:31 +0800

    动态加载控制器

--- DWWB/DWWB/Classes/Main(主要)/MainViewController.swift
+++ DWWB/DWWB/Classes/Main(主要)/MainViewController.swift
@@ -14,24 +14,90 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        addChildViewController(HomeViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(HomeViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(HomeViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(HomeViewController(), title: "我", imageName: "tabbar_profile")
+//        1.获取json文件路径
+        guard let jsonPath = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil) else {
+            print("没有获取到对应的文件路径")
+            return
+        }
 
+//        2.读取json文件中的内容
+        guard let jsonData = NSData(contentsOfFile: jsonPath) else {
+            print("没有获取到json文件中数据")
+            return
+        }
+
+//        3.将Data转成数组
+        guard let anyObject = try? NSJSONSerialization.JSONObjectWithData(jsonData, options: .MutableContainers) else {
+            return
+        }
+
+//        guard let dictArray = anyObject as? [[String : AnyObject]] else {
+//            return
+//        }
+
+
+        guard let dictArray = anyObject as? [[String : AnyObject]] else {
+            return
+        }
+
+//        4.遍历字典,获取对应的信息
+        for dict in dictArray {
+//            4.1获取控制器的对应的字符串
+            guard let vcName = dict["vcName"] as? String else {
+                continue
+            }
+
+//            4.2获取控制器显示的title
+            guard let title = dict["title"] as? String else {
+                return
+            }
+
+//            4.3获取控制器显示的图标名称
+            guard let imageName = dict["imageName"] as? String else {
+                return
+            }
+
+//            4.4添加子控制器
+            addChildViewController(vcName, title: title, imageName: imageName)
+
+        }
+
+       
     }
 
-    private func addChildViewController(childVc: UIViewController, title : String, imageName : String) {
+    private func addChildViewController(childVcName: String, title : String, imageName : String) {
 
-//        1.设置子控制器的属性
+//        0.获取命名空间
+        guard let nameSpace = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as? String else {
+            print("没有获取命名空间")
+            return
+        }
+
+//        1.根据字符串获取对应的Class
+        guard let ChildVcClass = NSClassFromString(nameSpace + "." + childVcName) else {
+            print("没有获取到字符串对应的class")
+            return
+        }
+
+
+//        2.将对应的AnyObject转成控制器的类型
+        guard let childVcType = ChildVcClass as? UIViewController.Type else {
+            print("没有获取到对应的控制器")
+            return
+        }
+
+//        3.创建对应的控制器对象
+        let childVc = childVcType.init()
+
+//        4.设置子控制器的属性
         childVc.title = title
         childVc.tabBarItem.image = UIImage(named: imageName)
         childVc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
 
-//        2.包装导航栏控制器
+//        5.包装导航栏控制器
         let childNav = UINavigationController(rootViewController: childVc)
 
-//        3.添加控制器
+//        6.添加控制器
         addChildViewController(childNav)
 
     }
