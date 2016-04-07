e78ad8cc3d666e528f00fe9f89f7552c1b88e94a
iOSreverse <iosdevhub@gmail.com>
2016-04-07 18:38:45 +0800

    1.更新SnapKit框架 pod 'SnapKit', '~> 0.18.0',Xcode7.2用最新版的会报错
    2.过期日期的处理

--- DWWB/DWWB/Classes/Main(主要)/OAuth/UserAccount.swift
+++ DWWB/DWWB/Classes/Main(主要)/OAuth/UserAccount.swift
@@ -13,9 +13,17 @@ class UserAccount: NSObject {
     ///授权AccessToken
     var access_token : String?
     ///过期时间-->秒
-    var expires_in : NSTimeInterval = 0.0
+    var expires_in : NSTimeInterval = 0.0 {
+        didSet {
+            expires_date = NSDate(timeIntervalSinceNow: expires_in)
+        }
+
+    }
     ///用户ID
     var uid : String?
+
+    ///过期日期
+    var expires_date : NSDate?
 
     // MARK: - 自定义构造函数
     init(dict : [String : AnyObject]) {
@@ -27,7 +35,41 @@ class UserAccount: NSObject {
 
         // MARK: - 重写description属性
         override var description : String {
-            return dictionaryWithValuesForKeys(["access_token", "expires_in", "uid"]).description
+            return dictionaryWithValuesForKeys(["access_token", "expires_date", "uid"]).description
 
     }
 }
+
+//import UIKit
+//
+//class UserAccount: NSObject {
+//    // MARK:- 属性
+//    /// 授权AccessToken
+//    var access_token : String?
+//    /// 过期时间-->秒
+//    var expires_in : NSTimeInterval = 0.0 {
+//        didSet {
+//            expires_date = NSDate(timeIntervalSinceNow: expires_in)
+//        }
+//    }
+//    /// 用户ID
+//    var uid : String?
+//
+//    /// 过期日期
+//    var expires_date : NSDate?
+//
+//
+//    // MARK:- 自定义构造函数
+//    init(dict : [String : AnyObject]) {
+//        super.init()
+//
+//        setValuesForKeysWithDictionary(dict)
+//    }
+//    override func setValue(value: AnyObject?, forUndefinedKey key: String) {}
+//
+//
+//    // MARK:- 重写description属性
+//    override var description : String {
+//        return dictionaryWithValuesForKeys(["access_token", "expires_date", "uid"]).description
+//    }
+//}
