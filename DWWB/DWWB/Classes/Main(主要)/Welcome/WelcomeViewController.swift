437d0d55f1373de4adf68125a16d1aab09344287
iOSreverse <iosdevhub@gmail.com>
2016-04-09 00:51:30 +0800

    布局欢迎界面

---
+++ DWWB/DWWB/Classes/Main(主要)/Welcome/WelcomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WelcomeViewController.swift
+//  DWWB
+//
+//  Created by xmg on 16/4/8.
+//  Copyright © 2016年 NewDee. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class WelcomeViewController: UIViewController {
+
+    // MARK: - 拖线的属性
+    @IBOutlet weak var iconViewBottomCons: NSLayoutConstraint!
+    @IBOutlet weak var iconView: UIImageView!
+
+    // MARK: - 系统回调函数
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        //0.设置头像
+        let profileURLString = UserAccountViewModel.shareIntance.account?.avatar_large
+
+        let url = NSURL(string: profileURLString ?? "")
+        iconView.sd_setImageWithURL(url, placeholderImage: UIImage(named: "avatar_default_big"))
+//            iconView.sd_setImageWithURL(url, placeholderImage: UIImage(named: "avatar_default_big"))
+
+        //1.改变约束的值
+        iconViewBottomCons.constant = UIScreen.mainScreen().bounds.height - 250
+
+        //2.执行动画
+        UIView.animateWithDuration(5.0, delay: 0.0, usingSpringWithDamping: 0.0001, initialSpringVelocity: 5.0, options: [], animations: { () -> Void in
+            self.view.layoutIfNeeded()
+            }) { (_) -> Void in
+        }
+    }
+}
