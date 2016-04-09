8965c1c29cf9b1efc42a23fea20d8dcce98fe36a
iOSreverse <iosdevhub@gmail.com>
2016-04-10 00:26:54 +0800

    对用户数据处理

--- DWWB/DWWB/Classes/Home(首页)/HomeModel/User.swift
+++ DWWB/DWWB/Classes/Home(首页)/HomeModel/User.swift
@@ -12,8 +12,35 @@ class User: NSObject {
     // MARK: - 属性
     var profile_image_url : String?      // 用户的头像
     var screen_name : String?            // 用户的昵称
-    var verified_type : Int = -1              // 用户的认证类型
-    var mbrank : Int = 0                 // 用户的会员等级
+
+    var verified_type : Int = -1 {        // 用户的认证类型
+    didSet {
+    switch verified_type {
+    case 0:
+    verifiedImage = UIImage(named: "avatar_vip")
+    case 2, 3, 5:
+    verifiedImage = UIImage(named: "avatar_enterprise_vip")
+    case 220:
+    verifiedImage = UIImage(named: "avatar_grassroot")
+    default:
+    verifiedImage = nil
+
+    }
+  }
+}
+    var mbrank : Int = 0  {               // 用户的会员等级
+        didSet {
+            if mbrank > 0 && mbrank <= 6 {
+                vipImage = UIImage(named: "common_button_white_disable")
+            }
+        }
+    }
+
+
+
+    // MARK: - 对用户数据处理
+    var verifiedImage : UIImage?
+    var vipImage : UIImage?
 
     // MARK: - 自定义构造函数
     init(dict : [String : AnyObject]) {
